597d03f58603076d8ece3cda7b160cf1e7ab4451
UAFederez <uafederez@gmail.com>
2022-12-29 18:36:32 +0800

    Sidenavigation bar component

---
+++ src/components/Navigation/SideNavigation.jsx
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom";
+
+const linkDefaultStyle = `w-full p-4 flex items-center gap-2 hover:bg-zinc-800 hover:text-zinc-50`;
+
+export default function SideNavigation({ user, links }) {
+    return (
+        <div className="__sidenav__ | flex flex-col items-start border-r w-48">
+            {links.map((link) => {
+                if (link.roles === "any" || link.roles.includes(user.role)) {
+                    return (
+                        <NavLink
+                            key={link.text}
+                            to={link.to}
+                            className={({ isActive }) =>
+                                isActive
+                                    ? `${linkDefaultStyle} bg-zinc-800 text-zinc-50 font-bold`
+                                    : `${linkDefaultStyle}`
+                            }
+                        >
+                            {link.icon} {link.text}
+                        </NavLink>
+                    );
+                }
+            })}
+        </div>
+    );
+}
