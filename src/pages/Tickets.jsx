25c837647f1593dab11e4210afb62844aced2429
UAFederez <uafederez@gmail.com>
2022-12-29 19:40:01 +0800

    Placeholder Tickets page

---
+++ src/pages/Tickets.jsx
@@ -0,0 +1,8 @@
+export default function Tickets() {
+    return (
+        <>
+            <h1 className="text-3xl font-bold">Tickets</h1>
+            <div className="w-full grid grid-cols-2 gap-4 mt-4"></div>
+        </>
+    );
+}
