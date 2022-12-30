d38fe5621fcf14f16dcabc41052f931ff3821021
UAFederez <uafederez@gmail.com>
2022-12-30 12:43:23 +0800

    Dropdown on the account section in the top navbar

---
+++ src/components/AccountDropdown.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function AccountDropdown({ user }) {
+    const [isDropdownVisible, setDropdownVisible] = useState(false);
+    return (
+        <>
+            <nav className="">
+                <button
+                    className="flex items-center gap-1"
+                    onClick={() => setDropdownVisible((prev) => !prev)}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                        />
+                    </svg>
+
+                    <p>
+                        {user.firstName} {user.lastName}
+                    </p>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-5 h-5"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                        />
+                    </svg>
+                </button>
+            </nav>
+            {isDropdownVisible && (
+                <div className="fixed right-0 top-14 w-48 border flex flex-col">
+                    <Link
+                        to="/my-account"
+                        className="p-4 hover:bg-zinc-800 hover:text-zinc-50 flex gap-2"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                            />
+                        </svg>
+                        Manage Account
+                    </Link>
+                    <Link
+                        to="/sign-out"
+                        className="p-4 hover:bg-zinc-800 hover:text-zinc-50 flex gap-2"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M5.636 5.636a9 9 0 1012.728 0M12 3v9"
+                            />
+                        </svg>
+                        Sign out
+                    </Link>
+                </div>
+            )}
+        </>
+    );
+}
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import AccountDropdown from "../components/AccountDropdown";
 import SideNavigation from "../components/Navigation/SideNavigation";
 import useCurrentUser from "../hooks/useCurrentUser";
 
@@ -135,7 +136,7 @@ export default function Root() {
 
     return (
         <>
-            <div className="__topbar__ | flex justify-between p-4 border-b fixed w-screen">
+            <header className="__topbar__ | flex justify-between p-4 border-b fixed w-screen">
                 <div className="__logo__ | flex gap-2 items-center">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -153,48 +154,15 @@ export default function Root() {
                     </svg>
                     <h1 className="font-bold">BugTracker</h1>
                 </div>
-                <div className="flex items-center gap-1">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-                        />
-                    </svg>
-
-                    <p>
-                        {user.firstName} {user.lastName}
-                    </p>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                        />
-                    </svg>
-                </div>
-            </div>
+                <AccountDropdown user={user} />
+            </header>
             <div className="__content__ | flex pt-14 min-h-screen">
                 <SideNavigation user={user} links={links} />
-                <div id="main-content" className="p-16 flex-1">
+                <main id="main-content" className="p-16 flex-1">
                     <div className="max-w-4xl mx-auto">
                         <Outlet />
                     </div>
-                </div>
+                </main>
             </div>
         </>
     );
