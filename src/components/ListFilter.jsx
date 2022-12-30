ba4571355d9c22a8c3ddc22b33ef0d856fa24f86
UAFederez <uafederez@gmail.com>
2022-12-30 22:19:32 +0800

    Component to display list for filter options in tickets page sidebar

---
+++ src/components/ListFilter.jsx
@@ -0,0 +1,57 @@
+export default function ListFilter({
+    list,
+
+    // The current state of all the filters
+    filters,
+
+    // The property of the ticket for filtering used to index into
+    // filters ("status", "severity", "project")
+    keyName,
+
+    // Can be null if the options are the values themselves, e.g., the text
+    // shown is "Resolved" but the value is also "Resolved"
+    idField,
+
+    // Can be null if the options to show
+    // the project name is shown on the list rather than the idField even though
+    // filter by the idField
+    displayField,
+
+    handleFilterChange,
+}) {
+    return (
+        <>
+            <div className="flex gap-1 items-center mb-1">
+                <input
+                    type="radio"
+                    value={"any"}
+                    onChange={(event) => handleFilterChange(event, keyName)}
+                    checked={filters[keyName].length === 0}
+                />
+                <label className="text-sm">Any {keyName}</label>
+            </div>
+            <div>
+                {list.map((item) => (
+                    <div
+                        key={idField ? item[idField] : item}
+                        className="flex gap-1 items-center"
+                    >
+                        <input
+                            type="checkbox"
+                            value={idField ? item[idField] : item}
+                            checked={filters[keyName].includes(
+                                idField ? item[idField] : item
+                            )}
+                            onChange={(event) =>
+                                handleFilterChange(event, keyName)
+                            }
+                        />
+                        <p className="text-sm">
+                            {displayField ? item[displayField] : item}
+                        </p>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
+}
