ae8718251c74731217a8c074306fecb254c86038
UAFederez <uafederez@gmail.com>
2022-12-30 22:17:59 +0800

    Changed positioning to sticky to maintain the necessary padding for the main content area

--- src/components/Navigation/SideNavigation.jsx
+++ src/components/Navigation/SideNavigation.jsx
@@ -5,23 +5,28 @@ const linkDefaultStyle = `w-full p-4 flex items-center gap-2 hover:bg-zinc-800 h
 export default function SideNavigation({ user, links }) {
     return (
         <div className="__sidenav__ | flex flex-col items-start border-r w-56">
-            {links.map((link) => {
-                if (link.roles === "any" || link.roles.includes(user.role)) {
-                    return (
-                        <NavLink
-                            key={link.text}
-                            to={link.to}
-                            className={({ isActive }) =>
-                                isActive
-                                    ? `${linkDefaultStyle} bg-zinc-800 text-zinc-50 font-bold`
-                                    : `${linkDefaultStyle}`
-                            }
-                        >
-                            {link.icon} {link.text}
-                        </NavLink>
-                    );
-                }
-            })}
+            <div className="sticky top-14 w-full">
+                {links.map((link) => {
+                    if (
+                        link.roles === "any" ||
+                        link.roles.includes(user.role)
+                    ) {
+                        return (
+                            <NavLink
+                                key={link.text}
+                                to={link.to}
+                                className={({ isActive }) =>
+                                    isActive
+                                        ? `${linkDefaultStyle} bg-zinc-800 text-zinc-50 font-bold`
+                                        : `${linkDefaultStyle}`
+                                }
+                            >
+                                {link.icon} {link.text}
+                            </NavLink>
+                        );
+                    }
+                })}
+            </div>
         </div>
     );
 }
